3e97634b1cef75f8109f69483f31cca922819098
dk9394 <k.deepak9394@gmail.com>
2025-05-13 07:53:44 +0530

    API service, Login and signup forms setup and other components

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { ApiService } from '../../shared/services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
+  standalone: false,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private apiService: ApiService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      emailId: this.fb.control('todd@gmail.com'),
+      password: this.fb.control('Todd@123'),
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      this.apiService.auth
+        .post('/login', this.loginForm.value)
+        .pipe()
+        .subscribe(
+          (response: any) => {
+            console.log('Login successful', response);
+          },
+          (error) => {
+            console.error('Login failed', error);
+          }
+        );
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  signupForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      firstName: this.fb.control('Deepak'),
+      lastName: this.fb.control('Kumar'),
+      emailId: this.fb.control('deepak@gmail.com'),
+      password: this.fb.control('Deepak@123'),
+      confirmPassword: this.fb.control('Deepak@123'),
+    });
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log(this.signupForm.value);
+      // Call your API service to handle the signup process
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
