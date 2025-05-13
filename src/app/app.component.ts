ed1045aef58d439e51fa47c58acbd64d077a916a
dk9394 <k.deepak9394@gmail.com>
2025-05-13 10:35:14 +0530

    base module to core module transfer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { BaseModule } from './base/base.module';
+import { CoreModule } from './core/core.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BaseModule],
+  imports: [RouterOutlet, CoreModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
